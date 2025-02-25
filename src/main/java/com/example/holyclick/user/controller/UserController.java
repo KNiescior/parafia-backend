c7ba1bd5ffe7f2b3b90ef6f233d9585c8c978b20
kniescior <27205@student.kpu.krosno.pl>
2025-02-25 19:03:44 +0100

    Wywal niech sie meczy xDD

--- src/main/java/com/example/holyclick/user/controller/UserController.java
+++ src/main/java/com/example/holyclick/user/controller/UserController.java
@@ -4,11 +4,7 @@ import com.example.holyclick.user.model.User;
 import com.example.holyclick.user.model.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(path="/demo")
